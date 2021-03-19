f5e3fc74bad424f152c346e536650be6f850572e
surella-jp <jp.surella0675@student.tsu.edu.ph>
2021-03-19 22:41:13 +0800

    Jp

---
+++ Employee.java
@@ -0,0 +1,39 @@
+
+
+ import java.util.Scanner;
+
+class E_Form {  
+
+    
+
+               int emp_id, salary;  
+
+               String name, address, department, email;  
+
+ 
+
+ 
+
+               public int getEmp_id() {  
+
+                              return emp_id;  
+
+               }  
+
+               public void setEmp_id(int emp_id) {  
+
+                              this.emp_id = emp_id;  
+
+               }  
+
+               public int getSalary() {  
+
+                              return salary;  
+
+               }  
+
+               public void setSalary(int salary) {  
+
+                              this.salary = salary;  
+
+               }
